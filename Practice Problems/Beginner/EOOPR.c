ffa66d513241fa5341722d91d86e2187e1549cca
Suvraneel Bhuin <63473496+Suvraneel@users.noreply.github.com>
2020-11-29 22:31:48 +0530

    Add files via upload
    The One Who Knocks!

---
+++ Practice Problems/Beginner/EOOPR.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int t, x, y, min;
+	scanf("%d", &t);
+	while (t--){
+	    scanf("%d %d", &x, &y);
+    printf("%d\n", step(x,y));
+	}
+	return 0;   
+}
+
+int step(int x, int y){
+    if (x < y) {
+      if ((y - x) % 2 == 0) {
+        if ((y - x) % 4 == 0) {
+          return 3;
+        } else {
+          return 2;
+        }
+      } else {
+        return 1;
+      }
+    } else if (x > y) {
+      if ((x - y) % 2 == 0) {
+        return 1;
+      } else {
+        return 2;
+      }
+    } else {
+      return 0;
+    }
+}
